e435f398f351316f8083ea68be1576ab8696af5f
Montoya Edu <montoya.edu@gmail.com>
2015-09-22 17:25:30 +0200

    Add solution file and other files required by visual studio. Add if statements to iwant.bat

--- iwant.bat
+++ iwant.bat
@@ -11,12 +11,24 @@ set TYPE=%5
 set ASSEMBLYVERSION=%6
 set ASSEMBLYVERSIONQUALIFIER=%7
 
+if "%TEMPLATE%"=="" set TEMPLATE=c#
+if "%NAME%"=="" set NAME=MyApp
+if "%PACKAGE%"=="" set PACKAGE=MyPackage
+if "%VERSION%"=="" set VERSION=v4.5
+if "%TYPE%"=="" set TYPE=WinExe
+if "%ASSEMBLYVERSION%"=="" set ASSEMBLYVERSION=1.0.0.0
+if "%ASSEMBLYVERSIONQUALIFIER%"=="" set ASSEMBLYVERSIONQUALIFIER=-SNAPSHOT
+
 Uuidgen.exe>tmp
 set /p PROJECTGUID=<tmp
 rm tmp
 
 Uuidgen.exe>tmp
 set /p COMGUID=<tmp
+rm tmp
+
+Uuidgen.exe>tmp
+set /p SOLUTIONGUID=<tmp
 rm tmp
 
 set TEMPLATEDIR=templates\%TEMPLATE%
@@ -29,6 +41,7 @@ echo using version %VERSION%
 echo using type %TYPE%
 echo using project guid %PROJECTGUID%
 echo using COM guid %COMGUID%
+echo using solution guid %SOLUTIONGUID%
 echo using assembly version %ASSEMBLYVERSION%
 echo using assembly version qualifier %ASSEMBLYVERSIONQUALIFIER%
 
@@ -44,15 +57,18 @@ echo s/${TargetFrameworkVersion}/%VERSION%/ >> tmp
 echo s/${OutputType}/%TYPE%/ >> tmp
 echo s/${ProjectGuid}/%PROJECTGUID%/ >> tmp
 echo s/${ComGuid}/%COMGUID%/ >> tmp
+echo s/${SolutionGuid}/%SOLUTIONGUID%/ >> tmp
 echo s/${AssemblyVersion}/%ASSEMBLYVERSION%/ >> tmp
 echo s/${AssemblyVersionQualifier}/%ASSEMBLYVERSIONQUALIFIER%/ >> tmp
 
+sed.exe -f tmp %FOLDER%\App_vs2010.sln>%FOLDER%\%PACKAGE%.%NAME%_vs2010.sln
 sed.exe -f tmp %FOLDER%\App.csproj>%FOLDER%\%PACKAGE%.%NAME%.csproj
 sed.exe -f tmp %FOLDER%\App.cs>%FOLDER%\%NAME%.cs
 sed.exe -f tmp %FOLDER%\Properties\AssemblyInfo.cstemplate>%FOLDER%\Properties\AssemblyInfo.cs
 sed.exe -f tmp %FOLDER%\pom.xmltemplate>%FOLDER%\pom.xml
 
 del /Q tmp
+del /Q %FOLDER%\App_vs2010.sln
 del /Q %FOLDER%\App.csproj
 del /Q %FOLDER%\App.cs
 del /Q %FOLDER%\Properties\AssemblyInfo.cstemplate

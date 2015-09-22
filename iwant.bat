0b4b4f59ff00fb66406d73659111eb511a1fc970
Montoya Edu <montoya.edu@gmail.com>
2015-09-22 18:05:23 +0200

    Fix app.config according to framework version

--- iwant.bat
+++ iwant.bat
@@ -1,7 +1,7 @@
 @echo off
 echo usage Examples
-echo iwant c# MyExecutable Com.Company.MySuite v4.5 WinExe 1.0.0.0 -SNAPSHOT
-echo iwant c# MyLibrary Com.Company.MySuite v4.5 Library 1.0.0.0 -SNAPSHOT
+echo iwant c# MyExecutable Com.Company.MySuite v4.0 WinExe 1.0.0.0 -SNAPSHOT
+echo iwant c# MyLibrary Com.Company.MySuite v4.0 Library 1.0.0.0 -SNAPSHOT
 
 set TEMPLATE=%1
 set NAME=%2
@@ -14,7 +14,7 @@ set ASSEMBLYVERSIONQUALIFIER=%7
 if "%TEMPLATE%"=="" set TEMPLATE=c#
 if "%NAME%"=="" set NAME=MyApp
 if "%PACKAGE%"=="" set PACKAGE=MyPackage
-if "%VERSION%"=="" set VERSION=v4.5
+if "%VERSION%"=="" set VERSION=v4.0
 if "%TYPE%"=="" set TYPE=WinExe
 if "%ASSEMBLYVERSION%"=="" set ASSEMBLYVERSION=1.0.0.0
 if "%ASSEMBLYVERSIONQUALIFIER%"=="" set ASSEMBLYVERSIONQUALIFIER=-SNAPSHOT
@@ -70,11 +70,14 @@ sed.exe -f tmp %FOLDER%\App.cs>%FOLDER%\%NAME%.cs
 sed.exe -f tmp %FOLDER%\Properties\AssemblyInfo.cstemplate>%FOLDER%\Properties\AssemblyInfo.cs
 sed.exe -f tmp %FOLDER%\pom.xmltemplate>%FOLDER%\pom.xml
 
+copy %FOLDER%\app_%VERSION%.configtemplate %FOLDER%\app.config
+
 del /Q tmp
 del /Q %FOLDER%\App_vs2010.sln
 del /Q %FOLDER%\App.csproj
 del /Q %FOLDER%\App.cs
 del /Q %FOLDER%\Properties\AssemblyInfo.cstemplate
 del /Q %FOLDER%\pom.xmltemplate
+del /Q %FOLDER%\*.configtemplate
 
 dir %FOLDER%

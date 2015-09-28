d045ef465116261e7386fb779605935d190ba3be
Montoya Edu <montoya.edu@gmail.com>
2015-09-28 23:22:20 +0200

    Add command for restoring nuget packages

---
+++ templates/c#/AppTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ${RootNamespace}
+{
+    [TestFixture]
+    public class ${AssemblyName}Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}

fe409a356851728064edde776a1e66cce0d9c587
Montoya Edu <montoya.edu@gmail.com>
2015-09-30 19:51:20 +0200

    Fix cover.sh

--- templates/java/cover.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn org.jacoco:jacoco-maven-plugin:prepare-agent clean install -Pcoverage-per-test
-mvn sonar:sonar -Dsonar.host.url=http://localhost/sonar -Dsonar.jdbc.url=jdbc:postgresql://localhost/sonar

9c687e0c62b443d105b913b843ab3ba5d0300769
Montoya Edu <montoya.edu@gmail.com>
2015-10-01 19:16:52 +0200

    Add release prepare scripts

---
+++ templates/c#/prepare.bat
@@ -0,0 +1,50 @@
+@echo on
+
+call mvn dotnet:release
+if ERRORLEVEL 1 goto Error
+
+set /p VERSION=< version.txt
+if ERRORLEVEL 1 goto Error
+
+call mvn versions:set -DnewVersion=%VERSION%
+if ERRORLEVEL 1 goto Error
+
+call mvn versions:commit
+if ERRORLEVEL 1 goto Error
+
+call mvn dotnet:version
+if ERRORLEVEL 1 goto Error
+
+git commit -a -m "[RELEASE] - released version %VERSION%"
+if ERRORLEVEL 1 goto Error
+
+git tag --file=version.txt %VERSION%
+if ERRORLEVEL 1 goto Error
+
+call mvn dotnet:next
+if ERRORLEVEL 1 goto Error
+
+set /p VERSION=< version.txt
+if ERRORLEVEL 1 goto Error
+
+call mvn versions:set -DnewVersion=%VERSION%
+if ERRORLEVEL 1 goto Error
+
+call mvn versions:commit
+if ERRORLEVEL 1 goto Error
+
+call mvn dotnet:version
+if ERRORLEVEL 1 goto Error
+
+git commit -a -m "[RELEASE] - new development version set to %VERSION%"
+if ERRORLEVEL 1 goto Error
+
+git push --all --follow-tags
+if ERRORLEVEL 1 goto Error
+
+goto:eof
+
+:Error
+echo Error. Verify Console Output.
+
+goto:eof

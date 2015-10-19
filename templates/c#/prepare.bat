5ef8800c46a5e34f5d9d4a703ea550c9538ba22c
Montoya Edu <montoya.edu@gmail.com>
2015-10-19 12:39:38 +0200

    fix script return value

--- templates/c#/prepare.bat
+++ templates/c#/prepare.bat
@@ -1,4 +1,6 @@
-@echo on
+@echo off
+
+echo "REMOVING -SNAPSHOT QUALIFIER FROM version.txt"
 
 call mvn dotnet:release
 if ERRORLEVEL 1 goto Error
@@ -6,17 +8,27 @@ if ERRORLEVEL 1 goto Error
 set /p VERSION=< version.txt
 if ERRORLEVEL 1 goto Error
 
+echo "CHANGING POM VERSION TO %VERSION%"
+
 call mvn versions:set -DnewVersion=%VERSION%
 if ERRORLEVEL 1 goto Error
+
+echo "ACCEPTING POM VERSION TO %VERSION%"
 
 call mvn versions:commit
 if ERRORLEVEL 1 goto Error
 
+echo "APPLYING NEW VERSION TO AssemblyInfo.cs %VERSION%"
+
 call mvn dotnet:version
 if ERRORLEVEL 1 goto Error
 
+echo "COMMITTING RELEASE %VERSION%"
+
 git commit -a -m "[RELEASE] - released version %VERSION%"
 if ERRORLEVEL 1 goto Error
+
+echo "TAGGING RELEASE %VERSION%"
 
 git tag --file=version.txt %VERSION%
 if ERRORLEVEL 1 goto Error
@@ -44,11 +56,16 @@ if ERRORLEVEL 1 goto Error
 git push --all --follow-tags
 if ERRORLEVEL 1 goto Error
 
+echo "PUBLISHING RELEASE %LATEST_TAG%"
+
 release.bat %LATEST_TAG%
 
 goto:eof
 
 :Error
 echo Error. Verify Console Output.
+EXIT /B 1
 
 goto:eof
+EXIT /B 0
+

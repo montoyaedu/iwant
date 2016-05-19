93f32afbb15f2e775ecce18f1d0860324cfcfb47
Montoya Edu <montoya.edu@gmail.com>
2016-05-19 20:13:40 +0200

    Add model for velocity context

---
+++ context.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "ProjectName" : "",
+    "RootNamespace" : "",
+    "TargetFrameworkVersion" : "",
+    "OutputFile" : "",
+    "ArtifactExtension" : "",
+    "Remote" : "",
+    "UserName" : "",
+    "Owner" : "",
+    "UserEmail" : "",
+    "WebServer" : "",
+    "ProjectGuid" : "",
+    "ComGuid" : "",
+    "SolutionGuid" : "",
+    "AssemblyName" : "",
+    "AssemblyVersion" : "",
+    "AssemblyVersionQualifier" : ""
+}
